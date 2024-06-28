1eafeed856557dc518026ea66fc74c7d93d485e9
Illia Kotviskyi <iiiako197@gmail.com>
2024-06-28 17:13:32 +0300

    EF Core configurations

--- src/Trendlink.Infrastructure/DependencyInjection.cs
+++ src/Trendlink.Infrastructure/DependencyInjection.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Quartz;
+using Trendlink.Application.Abstractions.Authentication;
 using Trendlink.Application.Abstractions.Caching;
 using Trendlink.Application.Abstractions.Clock;
 using Trendlink.Application.Abstractions.Data;
 using Trendlink.Domain.Abstraction;
+using Trendlink.Domain.Users;
+using Trendlink.Domain.Users.Cities;
+using Trendlink.Domain.Users.Countries;
+using Trendlink.Infrastructure.Authentication;
+using Trendlink.Infrastructure.Authorization;
 using Trendlink.Infrastructure.Caching;
 using Trendlink.Infrastructure.Clock;
 using Trendlink.Infrastructure.Data;
 using Trendlink.Infrastructure.Outbox;
+using Trendlink.Infrastructure.Repositories;
+using AuthenticationOptions = Trendlink.Infrastructure.Authentication.AuthenticationOptions;
+using AuthenticationService = Trendlink.Infrastructure.Authentication.AuthenticationService;
+using IAuthenticationService = Trendlink.Application.Abstractions.Authentication.IAuthenticationService;
 
 namespace Trendlink.Infrastructure
 {
@@ -23,6 +37,10 @@ namespace Trendlink.Infrastructure
             services.AddTransient<IDateTimeProvider, DateTimeProvider>();
 
             AddPersistence(services, configuration);
+
+            AddAuthentication(services, configuration);
+
+            AddAuthorization(services);
 
             AddCaching(services, configuration);
 
@@ -46,6 +64,9 @@ namespace Trendlink.Infrastructure
                     .UseSnakeCaseNamingConvention()
             );
 
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ICityRepository, CityRepository>();
+
             services.AddScoped<IUnitOfWork>(serviceProvider =>
                 serviceProvider.GetRequiredService<ApplicationDbContext>()
             );
@@ -53,6 +74,59 @@ namespace Trendlink.Infrastructure
             services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(
                 connectionString
             ));
+
+            SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
+        }
+
+        public static void AddAuthentication(
+            IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+
+            services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication"));
+
+            services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+            services.Configure<KeycloakOptions>(configuration.GetSection("Keycloak"));
+
+            services.AddTransient<AdminAuthorizationDelegatingHandler>();
+
+            services
+                .AddHttpClient<IAuthenticationService, AuthenticationService>(
+                    (serviceProvider, httpClient) =>
+                    {
+                        KeycloakOptions keycloakOptions = serviceProvider
+                            .GetRequiredService<IOptions<KeycloakOptions>>()
+                            .Value;
+
+                        httpClient.BaseAddress = new Uri(keycloakOptions.AdminUrl);
+                    }
+                )
+                .AddHttpMessageHandler<AdminAuthorizationDelegatingHandler>();
+
+            services.AddHttpClient<IJwtService, JwtService>(
+                (serviceProvider, httpClient) =>
+                {
+                    KeycloakOptions keycloakOptions = serviceProvider
+                        .GetRequiredService<IOptions<KeycloakOptions>>()
+                        .Value;
+
+                    httpClient.BaseAddress = new Uri(keycloakOptions.TokenUrl);
+                }
+            );
+
+            services.AddHttpContextAccessor();
+
+            services.AddScoped<IUserContext, UserContext>();
+        }
+
+        private static void AddAuthorization(IServiceCollection services)
+        {
+            services.AddScoped<AuthorizationService>();
+
+            services.AddTransient<IClaimsTransformation, CustomClaimsTransformation>();
         }
 
         private static void AddCaching(IServiceCollection services, IConfiguration configuration)
