d052d625494d59a0cefcbf08c857b631442eb30b
Illia Kotviskyi <iiiako197@gmail.com>
2024-07-17 17:29:46 +0300

    Advertisement and Condition domain unit tests

---
+++ test/Trendlink.Domain.UnitTests/Advertisements/AdvertisementTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Trendlink.Domain.Abstraction;
+using Trendlink.Domain.Conditions.Advertisements;
+using Trendlink.Domain.Conditions.Advertisements.ValueObjects;
+using Trendlink.Domain.Conditions.ValueObjects;
+
+namespace Trendlink.Domain.UnitTests.Advertisements
+{
+    public class AdvertisementTests
+    {
+        [Fact]
+        public void Create_Should_SetPropertyValues()
+        {
+            // Act
+            Advertisement advertisement = Advertisement
+                .Create(
+                    AdvertisementData.ConditionId,
+                    AdvertisementData.Name,
+                    AdvertisementData.Price,
+                    AdvertisementData.Description
+                )
+                .Value;
+
+            // Assert
+            advertisement.ConditionId.Should().Be(AdvertisementData.ConditionId);
+            advertisement.Name.Should().Be(AdvertisementData.Name);
+            advertisement.Price.Should().Be(AdvertisementData.Price);
+            advertisement.Description.Should().Be(AdvertisementData.Description);
+            advertisement.Condition.Should().BeNull();
+        }
+
+        [Fact]
+        public void Create_Should_ReturnFailure_When_NameIsNull()
+        {
+            // Act
+            Result<Advertisement> result = Advertisement.Create(
+                AdvertisementData.ConditionId,
+                new Name(null!),
+                AdvertisementData.Price,
+                AdvertisementData.Description
+            );
+
+            // Assert
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(AdvertisementErrors.Invalid);
+        }
+
+        [Fact]
+        public void Create_Should_ReturnFailure_When_DescriptionIsInvalid()
+        {
+            // Act
+            Result<Advertisement> result = Advertisement.Create(
+                AdvertisementData.ConditionId,
+                AdvertisementData.Name,
+                AdvertisementData.Price,
+                new Description(string.Empty)
+            );
+
+            // Assert
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(AdvertisementErrors.Invalid);
+        }
+
+        [Fact]
+        public void Create_Should_ReturnFailure_When_PriceIsInvalid()
+        {
+            // Act
+            Result<Advertisement> result = Advertisement.Create(
+                AdvertisementData.ConditionId,
+                AdvertisementData.Name,
+                Money.Zero(),
+                AdvertisementData.Description
+            );
+
+            // Assert
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(AdvertisementErrors.InvalidPrice);
+        }
+    }
+}
---
+++ test/Trendlink.Domain.UnitTests/Conditions/ConditionTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Trendlink.Domain.Abstraction;
+using Trendlink.Domain.Conditions;
+using Trendlink.Domain.Conditions.ValueObjects;
+
+namespace Trendlink.Domain.UnitTests.Conditions
+{
+    public class ConditionTests
+    {
+        [Fact]
+        public void Create_Should_SetPropertyValues()
+        {
+            // Act
+            Condition condition = Condition
+                .Create(ConditionData.UserId, ConditionData.Description)
+                .Value;
+
+            // Assert
+            condition.UserId.Should().Be(ConditionData.UserId);
+            condition.Description.Should().Be(ConditionData.Description);
+            condition.Advertisements.Should().BeEmpty();
+            condition.User.Should().BeNull();
+        }
+
+        [Fact]
+        public void Create_Should_ReturnFailure_WhenDescriptionIsNull()
+        {
+            // Act
+            Result<Condition> result = Condition.Create(
+                ConditionData.UserId,
+                new Description(null!)
+            );
+
+            // Assert
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(ConditionErrors.InvalidDescription);
+        }
+    }
+}
