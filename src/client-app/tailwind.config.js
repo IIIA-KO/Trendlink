c45951afd4216090d9a36445a5589ab05230b185
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2024-08-27 19:14:32 +0300

    Add main colors

--- src/client-app/tailwind.config.js
+++ src/client-app/tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
@@ -8,8 +9,16 @@ export default {
       },
       fontWeight: {
         light: '300',
-        normal: '400',
+        normal: '450',
         bold: '700',
+      },
+      colors: {
+        primary: "#009EA0",
+        hover: "#21CBCD",
+        background: "#FFFFFF",
+        textPrimary: "#FFFFFF",
+        textSecondary: "#3C3C3C",
+        // и т.д.
       },
     },
   },
