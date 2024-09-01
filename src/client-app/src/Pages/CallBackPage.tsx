3a0a50562813dd6131ed8801d3157ffd3ef0feb9
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2024-09-01 20:05:35 +0300

    Add navigate to login page

--- src/client-app/src/Pages/CallBackPage.tsx
+++ src/client-app/src/Pages/CallBackPage.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-
 import imgTwo from "../assets/img2CallBackPage.svg";
+import {useNavigate} from "react-router-dom";
 
 const CallBackPage: React.FC = () => {
+
+    const navigate = useNavigate();
+
+    const handleclick = () => {
+        navigate("/login");
+    }
+
     return (
         <div className="bg-background flex justify-between items-center h-screen w-screen">
             <div className="pb-64 max-w-xl w-full">
                 <div className="text-center">
-                    <h1 className="text-center text-[1rem] font-kodchasan font-bold mb-[1.5rem]">TrendLink</h1>
+                    <h1 className="text-center text-[1rem] font-kodchasan font-bold text-textSecondary mb-[1.5rem]">TrendLink</h1>
                     <p className="text-center text-[1.50rem] font-inter font-regular text-textSecondary mb-8">
                         Registration was successful!
                     </p>
                     <button
                         type="submit"
                         className="w-[310px] h-[47px] py-2 border-2 border-primary bg-primary text-textPrimary text-[1rem] mt-4 rounded-full transition duration-500 ease-in-out hover:bg-hover hover:border-hover hover:scale-105 active:scale-90 active:bg-transparent active:border-primary active:text-textSecondary focus:scale-100 transform"
+                        onClick={handleclick}
                     >
                         Continue
                     </button>
