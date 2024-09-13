a701aefc5323cb627ff6d14a59a55fc09fb58a17
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2024-09-13 22:43:08 +0300

    Delete home page path

--- src/client-app/src/App.tsx
+++ src/client-app/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import AuthPage from "./pages/AuthPage";
-import HomePage from "./pages/HomePage";
 import ProtectedRoute from './components/ProtectedRoute';
 import CallBackPage from "./pages/CallBackPage";
 import '@fontsource/kodchasan';
