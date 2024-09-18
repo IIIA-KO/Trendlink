9db2a9b40f1e6b774b8c3f5de1e0426bbac52272
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2024-09-18 22:23:20 +0300

    Add handle error

--- src/client-app/src/api/authApi.ts
+++ src/client-app/src/api/authApi.ts
@@ -1,6 +1,8 @@
 import axiosInstance from './Api';
 import { UserType } from '../models/UserType';
 import { AuthResponseType } from '../models/AuthResponseType'
+import axios from "axios";
+import {handleError} from "../helpers/handleError";
 
 export const register = async (data: {
     firstName: string;
@@ -11,32 +13,63 @@ export const register = async (data: {
     password: string;
     stateId: string;
 }): Promise<AuthResponseType> => {
-    const response = await axiosInstance.post('/users/register', data);
-    return {
-        accessToken: response.data.accessToken,
-        refreshToken: response.data.refreshToken,
-        expiresIn: 900,
-    };
+    try {
+        const response = await axiosInstance.post('/users/register', data);
+        return {
+            accessToken: response.data.accessToken,
+            refreshToken: response.data.refreshToken,
+            expiresIn: response.data.expiresIn,
+        };
+    } catch (error: unknown) {
+        handleError(error);
+        return {
+            accessToken: '',
+            refreshToken: '',
+            expiresIn: 0,
+        };
+    }
 };
 
 export const login = async (credentials: UserType): Promise<AuthResponseType> => {
-    const response = await axiosInstance.post('/users/login', credentials);
-    return {
-        accessToken: response.data.accessToken,
-        refreshToken: response.data.refreshToken,
-        expiresIn: 900,
-    };
+    try {
+        const response = await axiosInstance.post('/users/login', credentials);
+        return {
+            accessToken: response.data.accessToken,
+            refreshToken: response.data.refreshToken,
+            expiresIn: response.data.expiresIn,
+        };
+    } catch (error: unknown) {
+        handleError(error);
+        return {
+            accessToken: '',
+            refreshToken: '',
+            expiresIn: 0,
+        };
+    }
 };
 
 export const logout = async (): Promise<void> => {
-    await axiosInstance.post('/users/logout');
+    try {
+        await axiosInstance.post('/users/logout');
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const refreshAccessToken = async (refreshToken: string): Promise<AuthResponseType> => {
-    const response = await axiosInstance.post('/users/refresh', { refreshToken });
-    return {
-        accessToken: response.data.accessToken,
-        refreshToken: response.data.refreshToken,
-        expiresIn: 900,
-    };
+    try {
+        const response = await axiosInstance.post('/users/refresh', { refreshToken });
+        return {
+            accessToken: response.data.accessToken,
+            refreshToken: response.data.refreshToken,
+            expiresIn: response.data.expiresIn,
+        };
+    } catch (error: unknown) {
+        handleError(error);
+        return {
+            accessToken: '',
+            refreshToken: '',
+            expiresIn: 0,
+        };
+    }
 };
--- src/client-app/src/api/countriesAndStatesApi.ts
+++ src/client-app/src/api/countriesAndStatesApi.ts
@@ -1,13 +1,25 @@
 import axiosInstance from './Api';
 import { CountryType  } from '../models/CountryType';
 import { StateType   } from '../models/StateType';
+import axios from "axios";
+import {handleError} from "../helpers/handleError";
 
 export const getCountries = async (): Promise<CountryType[]> => {
-    const response = await axiosInstance.get('/countries');
-    return response.data;
+    try {
+        const response = await axiosInstance.get('/countries');
+        return response.data;
+    } catch (error: unknown) {
+        handleError(error);
+        return [];
+    }
 };
 
 export const getStates = async (countryId: string): Promise<StateType[]> => {
-    const response = await axiosInstance.get(`/countries/${countryId}/states`);
-    return response.data;
+    try {
+        const response = await axiosInstance.get(`/countries/${countryId}/states`);
+        return response.data;
+    } catch (error: unknown) {
+        handleError(error);
+        return [];
+    }
 };
