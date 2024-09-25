e918952cc106a09682a3e3ecb6fcc9592548ec13
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2024-09-25 21:37:52 +0300

    Update Loading page design

--- src/client-app/src/pages/LoadingPage.tsx
+++ src/client-app/src/pages/LoadingPage.tsx
@@ -3,11 +3,10 @@ import React from "react";
 const LoadingPage: React.FC = () => {
     return (
         <div className="bg-background flex justify-center items-center h-screen w-screen">
-            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-                <div className="mx-auto max-w-screen-sm text-center">
-                    <h1 className="mb-4 text-8xl tracking-tight font-inter font-bold lg:text-9xl text-transparent" style={{WebkitTextStroke: "2px #009EA0",}}>
-                        Loading....
-                    </h1>
+            <div className="relative">
+                <div className="spinner relative h-[113px] w-[113px] rounded-full bg-gradient-to-r from-teal-500 via-transparent to-transparent" />
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <p className="text-main-black font-kodchasan font-bold text-[12px]">Loading</p>
                 </div>
             </div>
         </div>
