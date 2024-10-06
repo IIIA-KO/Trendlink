1ee45f9c15ed3297dc752dcf7140bb31ba90da3d
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2024-10-06 21:25:07 +0300

    Add profile page version v2

---
+++ src/client-app/src/pages/ProfileV2Page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const ProfileV2Page: React.FC = () => {
+
+    return (
+        <div className="bg-background flex justify-between items-center h-screen w-screen">
+            <Navbar />
+        </div>
+    );
+};
+
+export default ProfileV2Page;
