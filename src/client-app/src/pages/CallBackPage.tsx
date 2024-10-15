2795e6430fa9528071447c502480fe7318003409
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2024-10-16 01:18:14 +0300

    Update google auth logic

--- src/client-app/src/pages/CallBackPage.tsx
+++ src/client-app/src/pages/CallBackPage.tsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import imgTwo from "../assets/img2CallBackPage.svg";
 import {useNavigate} from "react-router-dom";
+import {loginWithGoogle, registerWithGoogle} from "../services/google";
+import useAuth from "../hooks/useAuth";
 
 const CallBackPage: React.FC = () => {
 
     const navigate = useNavigate();
+    const { login } = useAuth();
+    const [message, setMessage] = useState("Loading...");
+
+    useEffect(() => {
+        const fetchToken = async () => {
+            const params =  new URLSearchParams(window.location.search);
+            const code = params.get('code');
+
+            const stateParams = params.get('state') ? new URLSearchParams(params.get('state') as string) : null;
+            const birthDate = stateParams?.get('birthDate') || '';
+            const phoneNumber = stateParams?.get('phoneNumber') || '';
+            const stateId = stateParams?.get('stateId') || '';
+            if (code) {
+                try {
+                    if (!birthDate || !phoneNumber || !stateId) {
+                        const data = await loginWithGoogle({ code });
+                        if (data) {
+                            localStorage.setItem('access_token', data.accessToken);
+                            login(data);
+                            setMessage('Login was successful!');
+                        } else {
+                            setMessage('Login failed. Please try again.');
+                        }
+                    } else {
+                        const data = await registerWithGoogle({ code, birthDate, phoneNumber, stateId });
+                        if (data) {
+                            localStorage.setItem('access_token', data.accessToken);
+                            login(data);
+                            setMessage('Registration was successful!');
+                        } else {
+                            setMessage('Registration failed. Please try again.');
+                        }
+                    }
+                } catch (error) {
+                    console.error('Error:', error);
+                    setMessage('An error occurred. Please try again later.');
+                }
+            } else {
+                setMessage('Authorization code not found.');
+            }
+        };
+
+        fetchToken();
+    }, [navigate, login]);
 
     const handleclick = () => {
-        navigate("/login");
+        navigate("/");
     }
 
     return (
@@ -16,7 +62,7 @@ const CallBackPage: React.FC = () => {
                 <div className="text-center">
                     <h1 className="text-center text-[1rem] font-kodchasan font-bold text-textSecondary mb-[1.5rem]">TrendLink</h1>
                     <p className="text-center text-[1.50rem] font-inter font-regular text-textSecondary mb-8">
-                        Registration was successful!
+                        {message}
                     </p>
                     <button
                         type="submit"
