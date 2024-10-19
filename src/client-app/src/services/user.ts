2796ae607e10cf8ce2a36cf2e4ee2cf16528c9fc
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2024-10-19 04:32:01 +0300

    Add Search Bloggers page

--- src/client-app/src/services/user.ts
+++ src/client-app/src/services/user.ts
@@ -1,10 +1,21 @@
 import {UserType} from "../types/UserType";
 import axiosInstance from "./api";
 import {handleError} from "../utils/handleError";
+import {UsersType} from "../types/UsersType";
 
 export const getUser = async (): Promise<UserType | null> => {
     try {
         const response = await axiosInstance.get('/users/me');
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return null;
+    }
+};
+
+export const getUsers = async (params: UsersType): Promise<UserType[] | null> => {
+    try {
+        const response = await axiosInstance.get('/users', { params });
         return response.data;
     } catch (error) {
         handleError(error);
