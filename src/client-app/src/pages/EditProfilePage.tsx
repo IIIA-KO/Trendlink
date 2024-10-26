b1261e19e46e191f2fd8cc1369b8844bcd7ae0f2
Sofia <yach.sophi@gmail.com>
2024-10-26 10:19:10 +0300

    Fixed edit

--- src/client-app/src/pages/EditProfilePage.tsx
+++ src/client-app/src/pages/EditProfilePage.tsx
@@ -4,7 +4,13 @@ import ProfileEditForm from "../components/Forms/ProfileEditForm";
 const EditProfilePage: React.FC = () => {
 
     return (
-        <ProfileEditForm />
+        <div className="w-full h-screen flex justify-center justify-start">
+        <div className="mr-[1100px] mt-[100px]">
+          <ProfileEditForm />
+        </div>
+      </div>
+      
+      
     );
 };
 
